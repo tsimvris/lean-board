b9bc3a1d26df9efdaef65faa439683ff2432093a
Petros Tsimvris <tsimvris@gmail.com>
2022-08-05 12:32:04 +0200

    added header and Add card button

---
+++ src/Components/LayoutComponents/Header.jsx
@@ -0,0 +1,10 @@
+import StyledHeader from "./StyledHeader";
+import StyledButton from "./StyledButton";
+
+export default function Header() {
+  return (
+    <StyledHeader>
+      <StyledButton>Add new Card</StyledButton>
+    </StyledHeader>
+  );
+}
---
+++ src/Components/LayoutComponents/StyledHeader.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+const StyledHeader = styled.header`
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 50px;
+  background: #335;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+export default StyledHeader;
